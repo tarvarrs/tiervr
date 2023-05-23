97aaaf2af18c06b66901e4badc5fefa3d7d67874
Waggerok <115732069+Waggerok@users.noreply.github.com>
2023-05-23 18:56:53 +0300

    May23 part2

---
+++ Assets/Scripts/Logics/EnemyAttack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private float = _attackRange;
+    public float AttackRange => _attackRange;
+
+    public void TryAttackPlayer()
+    {
+    }
+}
